2449c28679af47143c36d573d9ffcc789aeaa461
JulioVianaDev <juliovianadev@gmail.com>
2021-12-21 01:08:50 -0300

    organizando as pastas

---
+++ function.js
@@ -0,0 +1,21 @@
+function fn(){
+    return 'Code here';
+}
+
+const arrowFn = ()=> 'Code here';
+const arrowFn2 = () =>{
+    //mais uma expressão
+    return 'Code here';
+}
+
+//funções tambem são objetos
+fn.prop = "posso criar propriedades";
+
+console.log(fn());
+console.log(fn.prop);
+
+//receber parametros   
+const  logValue = value => console.log(value);
+const logFnResult = fnParam => console.log(fnParam);
+
+logFnResult(fn);
